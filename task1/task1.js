f54c6136022f4f5762ab1cba54a46ae68a3f1788
Isaev Nikita <worldhero142@gmail.com>
2023-12-01 20:04:28 +0700

    Работа в файле task1.js

--- task1/task1.js
+++ task1/task1.js
@@ -3,14 +3,9 @@
 // Продемонстрировать ее работу для трех треугольников.
 
 // Подключаем модуль для работы с консолью
-// Модуль node:readline предоставляет интерфейс для чтения данных из потока Readable (например, process.stdin) по одной строке за раз.
-const readline = require('readline'); // Require - это библиотека JavaScript и загрузчик файлов, который управляет зависимостями между файлами JavaScript и модульным программированием.
-// Создание интерфейса
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout }); //
+const prompt = require('prompt-sync')({ sigint: true });
 function f(a, b) {
     return Math.sqrt(a ** 2 + b ** 2);
 }
-rl.question('Введите длину двух катетов (через пробел): ', (action) => {
-    console.log(f(Number(action.split(' ')[0]), Number(action.split(' ')[1])));
-    rl.close();
-});
+action = prompt('Введите длину двух катетов (через пробел): ');
+console.log(f(Number(action.split(' ')[0]), Number(action.split(' ')[1])));
